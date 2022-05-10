850849ec541d086b33c740447b2e7f025185c316
Waldemar Głaz <w.glaz@cheil.com>
2022-05-10 18:59:11 +0200

    statement, footer >1440px

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import Counter from "./components/main/Counter";
 import Main from "./components/main/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Hero />
+      <div className="flex-container">
+        <Hero />
+        <div className="counters padding-container">
+          <Counter number="2K+" description="companies" />
+          <Counter number="8" description="languages" />
+          <Counter number="1.2M" description="leads" />
+        </div>
+      </div>
+
       <Main />
       <Footer />
     </div>
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,16 +1,11 @@
 import React from "react";
-import Counter from "./Counter";
+// import Counter from "./Counter";
 import Statement from "./Statement";
 import "./Main.css";
 
 const Main = () => {
   return (
     <main className="main">
-      <div className="counters padding-container">
-        <Counter number="2K+" description="companies" />
-        <Counter number="8" description="languages" />
-        <Counter number="1.2M" description="leads" />
-      </div>
       <Statement />
     </main>
   );
